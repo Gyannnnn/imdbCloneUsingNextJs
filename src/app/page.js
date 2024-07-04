04a0d267b5b2f3c2e57a21678bc28267468b72e0
Gyanranjan Patra 🤖 <153155648+Gyannnnn@users.noreply.github.com>
2024-07-04 12:26:05 +0530

    Styled Home Section

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div class="bg-zinc-900 h-screen w-full">Home</div>
+    <div class="bg-zinc-900 h-screen w-full text-white font-bold text-3xl">Home</div>
   )
 }
