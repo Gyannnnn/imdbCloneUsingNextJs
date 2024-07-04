96de1e06b41a3d37a0d1ec8e5dc5ed97285b53b0
Gyanranjan Patra 🤖 <153155648+Gyannnnn@users.noreply.github.com>
2024-07-04 16:13:49 +0530

    About Page

---
+++ imdbclocne/src/app/about/page.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="flex flex-col gap-10  max-w-6xl mx-auto px-2 py-36">
+      <h1 className="text-3xl text-blue-600 font-bold">About</h1>
+      <p className="sm:text-xl text-sm text-wrap text-left">
+        Welcome to my movie data base app i am Gyanranjan Patra an undergrad of
+        Veer Surendra Sai University Of Technology, I Have Created A One Srop
+        Solution For Movie-enthusiast and For All Your Needs
+      </p>
+      <p className="sm:text-xl text-sm text-wrap text-left">
+        This Website Is Specially Designed To Provide You A comprehensive
+        Information About All Types Of Movies from All Around The World , ALong
+        With The LAtest news, reviews and Trailers . My Movie App is Constantly
+        Getting Updated with The New Release Such That You Get Updated With This
+        Versetile Movie World . You Can Search For Movies By Title , Director ,
+        Actor , Genere Or Release Date , Making It Easy for finding Movies To
+        Watch In Your Leuisere Time .
+      </p>
+    </div>
+  );
+}
