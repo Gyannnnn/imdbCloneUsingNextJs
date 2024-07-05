8c8ba6371703416af956a13388ba72b4a4f7fcc2
Gyanranjan Patra 🤖 <153155648+Gyannnnn@users.noreply.github.com>
2024-07-05 19:13:46 +0530

    API

--- imdbclocne/src/app/page.js
+++ imdbclocne/src/app/page.js
@@ -1,13 +1,17 @@
-import React from 'react'
+"use client"
+
+import React, { useState }  from 'react'
 import Results from '@/components/Results';
+
 const API_KEY = process.env.API_KEY
 
 export default async function page({searchParams}) {
+  const[page,setPage]=useState(1)
   const genre = searchParams.genre ||'fetchTrending';
   const res = await fetch(
     `https://api.themoviedb.org/3${
       genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
-    }?api_key=${API_KEY}&language=en-US&region=IN&page=1`,
+    }?api_key=${API_KEY}&language=en-IN&region=IN&page=${page===0?1:page}`,
     
   );
   const data = await res.json();
@@ -17,8 +21,12 @@ export default async function page({searchParams}) {
   const results = data.results;
   console.log(results)
   return (
-    <div>
+    <div className="w-full min-h-[80vh] bg-white dark:bg-black flex flex-col items-center  justify-center">
       <Results results={results}/>
+      <div className="w-96 flex justify-center gap-10 pb-10">
+        <button onClick={(prevPage)=>setPage(prevPage=>prevPage-1)} className="px-4 py-1 rounded-md border" >Previous</button>
+        <button onClick={(prevPage)=>setPage(prevPage=>prevPage+1)} className="px-8 py-1 rounded-md bg-blue-600" >Next</button>
+      </div>
     </div>
   )
 }
--- imdbclocne/src/components/Results.jsx
+++ imdbclocne/src/components/Results.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
+import Image from "next/image";
 
 export default function Results({ results }) {
   return (
-    <div>
+    <div className="w-[80vw] min-h-[100vh]  grid grid-cols-4 gap-2 pt-3 pb-3 justify-items-center ">
       {results.map((result) => (
-        <div key={result.id}>
-          <h2>{result.original_title}</h2>
+        <div
+          className="w-60 min-h-96 bg-zinc-600 rounded-md p-1"
+          key={result.id}
+        >
+          <Image
+            className="rounded-md"
+            src={`https://image.tmdb.org/t/p/w500/${result.poster_path}`}
+            width={500}
+            height={500}
+            alt="Picture of the author"
+          />
+          <div>
+            <h1>{result.title}</h1>
+            <h2>{result.release_date}</h2>
+            <h3>{result.overview}</h3>
+          </div>
         </div>
       ))}
     </div>
