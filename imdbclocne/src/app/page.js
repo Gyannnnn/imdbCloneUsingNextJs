73c4fd0004f65c482d4b814a8f0c4736bfdf8d61
Gyanranjan Patra 🤖 <153155648+Gyannnnn@users.noreply.github.com>
2024-07-05 14:20:10 +0530

    Integrated Api

--- imdbclocne/src/app/page.js
+++ imdbclocne/src/app/page.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import Results from '@/components/Results';
+const API_KEY = process.env.API_KEY
 
-export default function page() {
+export default async function page({searchParams}) {
+  const genre = searchParams.genre ||'fetchTrending';
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=en-US&region=IN&page=1`,
+    
+  );
+  const data = await res.json();
+  if(!res.ok){
+    throw new Error("Faild TO Fetch Data");
+  }
+  const results = data.results;
+  console.log(results)
   return (
-    <div>page</div>
+    <div>
+      <Results results={results}/>
+    </div>
   )
 }
