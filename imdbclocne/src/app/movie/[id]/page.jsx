aabce11777eddbb4fdda06678d4d0a66ebf6ba37
Gyanranjan Patra 🤖 <153155648+Gyannnnn@users.noreply.github.com>
2024-07-07 20:27:24 +0530

    Updated

---
+++ imdbclocne/src/app/movie/[id]/page.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Image from "next/image";
+
+export default async function moviePage({ params }) {
+  const movieId = params.id;
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.API_KEY}`
+  );
+  const movie = await res.json();
+  console.log(movie);
+  return (
+    <div className="w-full h-[80vh] flex items-center justify-center">
+      <div className="w-full h-[40vh] flex px-32 gap-3">
+        <Image
+          src={`https://image.tmdb.org/t/p/${movie.poster_path || movie.backdrop_path}`}
+          width={500}
+          height={300}
+          
+        />
+        <div>
+          <h2>{movie.original_title}</h2>
+          <p>{movie.overview}</p>
+          <p>{movie.release_date}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- imdbclocne/src/components/Results.jsx
+++ imdbclocne/src/components/Results.jsx
@@ -1,34 +1,40 @@
 import React from "react";
 import Image from "next/image";
 import { AiOutlineLike } from "react-icons/ai";
+import Link from "next/link";
 export default function Results({ results }) {
   return (
     <div className="w-[80vw] min-h-[100vh]  grid grid-cols-4 gap-6 pt-10 pb-3 justify-items-center max-sm:grid-cols-1 max-lg:grid-cols-2 max-xl:grid-cols-3 ">
       {results.map((result) => (
-        <div className="w-60 min-h-96 bg-zinc-600 rounded-md border-2 border-blue-600 max-sm:w-[90vw]" key={result.id}>
-          <Image
-            className="rounded-md"
-            src={`https://image.tmdb.org/t/p/w500/${result.poster_path}`}
-            width={500}
-            height={500}
-            alt="Picture of the author"
-          />
-          <div>
-            <h1 className="text-white font-bold text-center text-md">
-              {result.original_title === "" ? Title : result.original_title}
-            </h1>
-            <h3 className="text-center text-sm font-light">
-              {result.overview.slice(0, 30) + "..."}
-            </h3>
-            <div className="w-60 flex justify-between px-4">
-              <h2 className="text-gray-400 text-sm">{result.release_date}</h2>
-              <div className="flex gap-2">
-                <AiOutlineLike className="text-2xl" />
-                <span>{result.vote_count}</span>
+        <Link href={`/movie/${result.id}`}>
+          <div
+            className="w-60 min-h-96 bg-zinc-600 rounded-md border-2 border-blue-600 max-sm:w-[90vw]"
+            key={result.id}
+          >
+            <Image
+              className="rounded-md"
+              src={`https://image.tmdb.org/t/p/w500/${result.poster_path}`}
+              width={500}
+              height={500}
+              alt="Picture of the author"
+            />
+            <div>
+              <h1 className="text-white font-bold text-center text-md">
+                {result.original_title || result.name}
+              </h1>
+              <h3 className="text-center text-sm font-light">
+                {result.overview.slice(0, 30) + "..."}
+              </h3>
+              <div className="w-60 flex justify-between px-4">
+                <h2 className="text-gray-400 text-sm">{result.release_date}</h2>
+                <div className="flex gap-2">
+                  <AiOutlineLike className="text-2xl" />
+                  <span>{result.vote_count}</span>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </Link>
       ))}
     </div>
   );
